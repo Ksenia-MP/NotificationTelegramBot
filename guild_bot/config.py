d036ca5c5bb88a67c2a836f04b8b8914becdb09b
Ksenia-MP <114159722+Ksenia-MP@users.noreply.github.com>
2026-03-22 16:18:49 +0400

    Update config.py

--- guild_bot/config.py
+++ guild_bot/config.py
@@ -6,20 +6,20 @@ from pathlib import Path
 class Config:
     """Все настройки бота в одном месте"""
     # Директории и пути к файлам расписания и логов для самоочиски
-    BASE_DIR = Path(__file__).parent.parent # Выше на 2 директории чем config.py (D:\MyProjects\>>Guild<<\guild_bot\config.py)
+    BASE_DIR = Path(__file__).parent.parent # Выше на 2 директории чем config.py (С:\Path\to\project\>>NotificationTelegramBot<<\guild_bot\config.py)
     IMAGES_DIR = os.path.join(BASE_DIR, "images")
     SCHEDULE_CSV = os.path.join(BASE_DIR, "schedule.csv")
     MESSAGE_LOG_JSON = os.path.join(BASE_DIR, "message_log.json")
 
-    BOT_TOKEN: str = "8745543448:AAGMklSx8e-8cnq2Ch4WC4HXBG4ugeSzu94"
-    CHAT_ID: int = -1003778217245
-    THREAD_ID: int = 3
-    TIME_SHIFT: int = -1
-    TIME_ZONE: str = "Europe/Moscow"
-    MORNING_DIGEST_HOUR: int = 11
+    BOT_TOKEN: str = "1234567890:ABCdefGHIjklMNOpqrsTUVwxyz"    # Поставить свой токен бота полученный от @BotFather
+    CHAT_ID: int = None        # Заменить на ID чата (формата -1234567890123), получить можно с помощью команды /test
+    THREAD_ID: int = None      # Заменить на ID группы (если требуется), получить можно с помощью команды /test
+    TIME_SHIFT: int = -1       # Сдвиг во времени относительно TIME_ZONE
+    TIME_ZONE: str = "Europe/Moscow"    # Часовой пояс МСК (уведомления из schedule.csv будут приходить по указоннуму времени в данном часовом поясе)
+    MORNING_DIGEST_HOUR: int = 11       # Время утреннего дайджеста
     MORNING_DIGEST_MINUTE: int = 00
     
-    # Пути к картинкам
+    # Пути к картинкам (можно добавить свое)
     IMAGE_PATHS = {
         'boss_1': os.path.join(IMAGES_DIR, "boss_6.jpg"),
         'boss_2': os.path.join(IMAGES_DIR, "boss_11.jpg"),
